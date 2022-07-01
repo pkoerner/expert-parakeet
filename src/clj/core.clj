0398d2e95445acfe7b31be58623d01b8920443fb
Raphael Peters <rappet@rappet.de>
2022-07-01 14:32:15 +0200

    TODO API middleware

--- src/clj/core.clj
+++ src/clj/core.clj
@@ -8,44 +8,29 @@
    [ring.middleware.params :refer [wrap-params]]
    [ring.middleware.reload :refer [wrap-reload]]
    [ring.middleware.oauth2 :refer [wrap-oauth2]]
-   [ring.util.request :refer [body-string]]
-   [clj-http.client :as client]))
-
-(defn extract-token [request] (get-in request [:oauth2/access-tokens :github :token]))
+   ;;[ring.middleware.defaults :refer [wrap-defaults, site-defaults]]
+   ;;[ring.middleware.session :refer [wrap-session]]
+   ;;[ring.middleware.session.cookie :refer [cookie-store]]
+   [auth :refer [extract-token oauth-github-profile wrap-authentication]]))
 
 (defroutes routes
   (GET "/api/random" [] (str [(rand-int 10) (rand-int 10)]))
   (GET "/api/random3" [] (str [(rand-int 10) (rand-int 10)]))
   (GET "/api/check-random" [a :<< as-int b :<< as-int res :<< as-int]
     (str (= (+ a b) res)))
-  ;;(GET "/api/access-token" request (str (-> request :ring.middleware.oauth2/access-tokens :github)))
   (GET "/api/access-token" request (str (extract-token request)))
-  (GET "/api/user" request
-    (str (get-in (client/get
-                  "https://api.github.com/user"
-                  {:accept :json
-                   :oauth-token (extract-token request)
-                   :as :json})
-                 [:body :email])))
+  (GET "/api/session" request (str (:session request)))
   (route/not-found "Not Found"))
 
-(def oauth-client-id (System/getenv "OAUTH_CLIENT_ID"))
-(def oauth-client-secret (System/getenv "OAUTH_CLIENT_SECRET"))
-
 (def app
-  (let [profiles {:github
-                  {:authorize-uri    "https://github.com/login/oauth/authorize"
-                   :access-token-uri "https://github.com/login/oauth/access_token"
-                   :client-id        oauth-client-id
-                   :client-secret    oauth-client-secret
-                   :scopes           ["user:email"]
-                   :launch-uri       "/api/oauth2/github"
-                   :redirect-uri     "/api/oauth2/github/callback"
-                   :landing-uri      "/api/access-token"}}]
-    (-> routes
-        (wrap-oauth2 profiles)
-        (wrap-defaults (assoc-in site-defaults [:session :cookie-attrs :same-site] :lax))
-        wrap-params)))
+  (-> routes
+      wrap-authentication
+      (wrap-oauth2 oauth-github-profile)
+      (wrap-defaults (assoc-in site-defaults [:session :cookie-attrs :same-site] :lax))
+        ;; wrap-defaults kann das irgend wie auch -> ging nur nicht 
+        ;;(wrap-session {:store (cookie-store {:key (byte-array (map byte [0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15])) :cookie-name "parakeet-session"})})
+        ;;(wrap-defaults (assoc-in site-defaults [:cookie-attrs :same-site] :lax))
+      wrap-params))
 
 
 (def app-dev (wrap-reload #'app))
