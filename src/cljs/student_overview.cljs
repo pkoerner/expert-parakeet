f8b7bb059cde9526396c08c7ad5f0194cfa10aff
Lian Remme <lirem101@hhu.de>
2022-07-26 12:02:44 +0800

    Feat: No headline that shows who is logged in our possibility to log out
    Implemented headline

--- src/cljs/student_overview.cljs
+++ src/cljs/student_overview.cljs
@@ -1,11 +1,27 @@
-(ns student-overview)
+(ns student-overview
+  (:require
+    [re-com.core :refer [at button h-box]]))
 
 
 ;; For now, we only look at user with id 0
 (def user-id 0)
 
 
+(defn headline
+  [user-id]
+  [:div
+   [h-box
+    :gap      "10px"
+    :children [[:div "Logged in as: " user-id]
+               [button
+                :src (at)
+                :class "button-primary"
+                ;; :on-click #(rf/dispatch [:logout])
+                :label "Logout"]]]])
+
+
 (defn main
   []
   [:div
-   [:h1 "Hello from Student Overview"]])
+   [:h1 "Hello from Student Overview"]
+   [headline user-id]])
