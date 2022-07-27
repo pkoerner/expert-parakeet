59aec99df2d744475a44276d84abff4b5004b264
Lian Remme <lirem101@hhu.de>
2022-07-27 15:00:53 +0800

    Fix: Events, Subs and hiccups were all in one file
    Took events and subs in extra file

--- src/cljs/student_overview.cljs
+++ src/cljs/student_overview.cljs
@@ -1,118 +1,13 @@
 (ns student-overview
   (:require
-    [ajax.core :refer [GET]]
-    [cljs.tools.reader.edn :as edn]
     [re-com.core :refer [at button h-box v-box title]]
-    [re-frame.core :as rf]))
+    [re-frame.core :as rf]
+    [student-overview-events]
+    [student-overview-subscriptions]))
 
 
 ;; For now, we only look at user with id 0
 (def user-id 0)
-
-
-(rf/reg-event-fx
-  :retrieve-kurse-for-this-student
-  (fn [{:keys [db]} [_ user-id]]
-    {:db db
-     :dispatch (GET (str "/api/kurse-from-user/" user-id)
-                    {:handler (fn [resp]
-                                (rf/dispatch [:update-kurse-for-this-student
-                                              (edn/read-string (str "[" resp "]"))]))})}))
-
-
-(rf/reg-event-fx
-  :retrieve-fach-for-this-kurs
-  (fn [{:keys [db]} [_ kurs-id]]
-    {:db db
-     :dispatch (GET (str "/api/fach-from-kurs/" kurs-id)
-                    {:handler (fn [resp]
-                                (rf/dispatch [:update-fach-for-this-kurs
-                                              [kurs-id (edn/read-string resp)]]))})}))
-
-
-(rf/reg-event-fx
-  :retrieve-tests-for-this-kurs
-  (fn [{:keys [db]} [_ kurs-id]]
-    {:db db
-     :dispatch (GET (str "/api/tests-from-kurs/" kurs-id)
-                    {:handler (fn [resp]
-                                (rf/dispatch [:update-tests-for-this-kurs
-                                              [kurs-id (edn/read-string (str "[" resp "]"))]]))})}))
-
-
-(rf/reg-event-fx
-  :retrieve-fragen-for-this-test
-  (fn [{:keys [db]} [_ test-id]]
-    {:db db
-     :dispatch (GET (str "/api/fragen-from-test/" test-id)
-                    {:handler (fn [resp]
-                                (rf/dispatch [:update-fragen-for-test
-                                              [test-id (edn/read-string (str "[" resp "]"))]]))})}))
-
-
-(rf/reg-event-fx
-  :retrieve-antwort-for-this-user-frage
-  (fn [{:keys [db]} [_ [user-id frage-id]]]
-    {:db db
-     :dispatch (GET (str "/api/antworten-from-user-frage/" user-id "/" frage-id)
-                    {:handler (fn [resp]
-                                (rf/dispatch [:update-antwort-for-this-user-frage
-                                              [user-id frage-id (edn/read-string (str "[" resp "]"))]]))})}))
-
-
-(rf/reg-event-db
-  :update-kurse-for-this-student
-  (fn [db [_ kurse]]
-    (assoc db :kurse kurse)))
-
-
-(rf/reg-event-db
-  :update-tests-for-this-kurs
-  (fn [db [_ [kurs-id tests]]]
-    (assoc db [:tests-by-kurs kurs-id] tests)))
-
-
-(rf/reg-event-db
-  :update-fach-for-this-kurs
-  (fn [db [_ [kurs-id fach]]]
-    (assoc db [:fach-by-kurs kurs-id] fach)))
-
-
-(rf/reg-event-db
-  :update-fragen-for-test
-  (fn [db [_ [test-id fragen]]]
-    (assoc db [:fragen-by-test test-id] fragen)))
-
-
-(rf/reg-event-db
-  :update-antwort-for-this-user-frage
-  (fn [db [_ [user-id frage-id antworten]]]
-    (assoc db [:antwort-by-user-frage user-id frage-id] antworten)))
-
-
-(rf/reg-sub
-  :kurse
-  (fn [db _] (:kurse db)))
-
-
-(rf/reg-sub
-  :fach-from-kurs
-  (fn [db [_ kurs-id]] (get db [:fach-by-kurs kurs-id])))
-
-
-(rf/reg-sub
-  :tests-from-kurs
-  (fn [db [_ kurs-id]] (get db [:tests-by-kurs kurs-id])))
-
-
-(rf/reg-sub
-  :fragen-from-test
-  (fn [db [_ test-id]] (get db [:fragen-by-test test-id])))
-
-
-(rf/reg-sub
-  :antwort-from-user-frage
-  (fn [db [_ user-id frage-id]] (get db [:antwort-by-user-frage user-id frage-id])))
 
 
 (defn headline
---
+++ src/cljs/student_overview_subscriptions.cljs
@@ -0,0 +1,28 @@
+(ns student-overview-subscriptions
+  (:require
+    [re-frame.core :as rf]))
+
+
+(rf/reg-sub
+  :kurse
+  (fn [db _] (:kurse db)))
+
+
+(rf/reg-sub
+  :fach-from-kurs
+  (fn [db [_ kurs-id]] (get db [:fach-by-kurs kurs-id])))
+
+
+(rf/reg-sub
+  :tests-from-kurs
+  (fn [db [_ kurs-id]] (get db [:tests-by-kurs kurs-id])))
+
+
+(rf/reg-sub
+  :fragen-from-test
+  (fn [db [_ test-id]] (get db [:fragen-by-test test-id])))
+
+
+(rf/reg-sub
+  :antwort-from-user-frage
+  (fn [db [_ user-id frage-id]] (get db [:antwort-by-user-frage user-id frage-id])))
