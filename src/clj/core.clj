d2870743d796aa556949d1cc172fdf26d5183d81
Lian Remme <lirem101@hhu.de>
2022-07-28 21:49:51 +0800

    Fix: Some GETS were not necessary anymore
    Removed them

--- src/clj/core.clj
+++ src/clj/core.clj
@@ -20,22 +20,12 @@
                 (response (db/all-tests)))
            (GET "/test/:id" [id :<< as-int]
                 (response (db/test-by-id id)))
-           (GET "/tests-from-kurs/:id" [id :<< as-int]
-                (str
-                  (-> {}
-                      (assoc :kurs-id id)
-                      (assoc :tests (into [] (db/tests-by-kurs-id id))))))
            ;; fragen
            ;; do we need these route, why can't we embed the questions in the test
            (GET "/frage" []
                 (response (db/all-fragen)))
            (GET "/frage/:id" [id :<< as-int]
                 (response (db/frage-by-id id)))
-           (GET "/fragen-from-test/:id" [id :<< as-int]
-                (str
-                  (-> {}
-                      (assoc :test-id id)
-                      (assoc :fragen (into [] (db/fragen-by-test-id id))))))
 
            (GET "/antwort" []
                 (str (db/all-antwort)))
@@ -45,24 +35,7 @@
            (POST "api/test/:test-id/antwort" [test-id :<< as-int antworten]
                  (prn antworten)
                  (db/add-antworten test-id antworten))
-           (GET "/antworten-from-user-frage/:uid/:fid" [uid :<< as-int fid :<< as-int]
-                (str
-                  (-> {}
-                      (assoc :user-id uid)
-                      (assoc :frage-id fid)
-                      (assoc :antworten (into [] (db/antworten-by-frage-user-id fid uid))))))
-           ;; kurse
-           (GET "/kurse-from-user/:id" [id :<< as-int]
-                (str
-                  (-> {}
-                      (assoc :user-id id)
-                      (assoc :kurse (into [] (db/kurse-by-user-id id))))))
-           ;; Faecher
-           (GET "/fach-from-kurs/:id" [id :<< as-int]
-                (str
-                  (-> {}
-                      (assoc :kurs-id id)
-                      (assoc :fach (db/fach-by-kurs-id id)))))
+
            ;; antworten
            ;; maybe better route /test/:test-id/antworten
            (POST "/test/:test-id/antwort" [test-id :<< as-int :as r]
