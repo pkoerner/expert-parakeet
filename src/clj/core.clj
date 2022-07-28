1f1dcec971709007d2461c2310412f695a02d2ca
Lian Remme <lirem101@hhu.de>
2022-07-28 13:17:35 +0800

    Fix: events had side-effects
    Implemented http-xhrio

--- src/clj/core.clj
+++ src/clj/core.clj
@@ -17,14 +17,20 @@
            (GET "/test/:id" [id :<< as-int]
                 (str (db/test-by-id id)))
            (GET "/tests-from-kurs/:id" [id :<< as-int]
-                (db/tests-by-kurs-id id))
+                (str
+                  (-> {}
+                      (assoc :kurs-id id)
+                      (assoc :tests (into [] (db/tests-by-kurs-id id))))))
            ;; fragen
            (GET "/frage" []
                 (str (db/all-fragen)))
            (GET "/frage/:id" [id :<< as-int]
                 (str (db/frage-by-id id)))
            (GET "/fragen-from-test/:id" [id :<< as-int]
-                (db/fragen-by-test-id id))
+                (str
+                  (-> {}
+                      (assoc :test-id id)
+                      (assoc :fragen (into [] (db/fragen-by-test-id id))))))
 
            (GET "/antwort" []
                 (str (db/all-antwort)))
@@ -35,19 +41,23 @@
                 (prn antworten)
                 (db/add-antwort test-id antworten))
            (GET "/antworten-from-user-frage/:uid/:fid" [uid :<< as-int fid :<< as-int]
-                (db/antworten-by-frage-user-id fid uid))
+                (str
+                  (-> {}
+                      (assoc :user-id uid)
+                      (assoc :frage-id fid)
+                      (assoc :antworten (into [] (db/antworten-by-frage-user-id fid uid))))))
            ;; kurse
            (GET "/kurse-from-user/:id" [id :<< as-int]
-                (prn (-> {}
-                         (assoc :user-id id)
-                         (assoc :kurse (into []  (db/kurs-by-user-id id)))))
                 (str
                   (-> {}
                       (assoc :user-id id)
-                      (assoc :kurse (into []  (db/kurs-by-user-id id))))))
+                      (assoc :kurse (into [] (db/kurs-by-user-id id))))))
            ;; Faecher
            (GET "/fach-from-kurs/:id" [id :<< as-int]
-                (db/fach-by-kurs-id id)))
+                (str
+                  (-> {}
+                      (assoc :kurs-id id)
+                      (assoc :fach (db/fach-by-kurs-id id))))))
   (route/not-found "Not Found"))
 
 
--- src/cljs/student_overview_events.cljs
+++ src/cljs/student_overview_events.cljs
@@ -1,6 +1,6 @@
 (ns student-overview-events
   (:require
-    [ajax.core :as ajax :refer [GET]]
+    [ajax.core :as ajax]
     [cljs.tools.reader.edn :as edn]
     [re-frame.core :as rf]))
 
@@ -18,51 +18,51 @@
                    :uri             (str "/api/kurse-from-user/" user-id)
                    :timeout         8000
                    :response-format (ajax/text-response-format)
-                   :on-success      [:update-kurse-for-this-student]}
-     #_#_:dispatch (GET (str "/api/kurse-from-user/" user-id)
-                    {:handler (fn [resp]
-                                (rf/dispatch [:update-kurse-for-this-student
-                                              (edn/read-string resp)]))})}))
+                   :on-success      [:update-kurse-for-this-student]}}))
 
 
 (rf/reg-event-fx
   :retrieve-fach-for-this-kurs
   (fn [{:keys [db]} [_ kurs-id]]
-    {:db db
-     :dispatch (GET (str "/api/fach-from-kurs/" kurs-id)
-                    {:handler (fn [resp]
-                                (rf/dispatch [:update-fach-for-this-kurs
-                                              [kurs-id (edn/read-string resp)]]))})}))
+    {:db          (assoc db :loading true)
+     :http-xhrio  {:method          :get
+                   :uri             (str "/api/fach-from-kurs/" kurs-id)
+                   :timeout         8000
+                   :response-format (ajax/text-response-format)
+                   :on-success      [:update-fach-for-this-kurs]}}))
 
 
 (rf/reg-event-fx
   :retrieve-tests-for-this-kurs
   (fn [{:keys [db]} [_ kurs-id]]
-    {:db db
-     :dispatch (GET (str "/api/tests-from-kurs/" kurs-id)
-                    {:handler (fn [resp]
-                                (rf/dispatch [:update-tests-for-this-kurs
-                                              [kurs-id (edn/read-string (str "[" resp "]"))]]))})}))
+    {:db          (assoc db :loading true)
+     :http-xhrio  {:method          :get
+                   :uri             (str "/api/tests-from-kurs/" kurs-id)
+                   :timeout         8000
+                   :response-format (ajax/text-response-format)
+                   :on-success      [:update-tests-for-this-kurs]}}))
 
 
 (rf/reg-event-fx
   :retrieve-fragen-for-this-test
   (fn [{:keys [db]} [_ test-id]]
-    {:db db
-     :dispatch (GET (str "/api/fragen-from-test/" test-id)
-                    {:handler (fn [resp]
-                                (rf/dispatch [:update-fragen-for-test
-                                              [test-id (edn/read-string (str "[" resp "]"))]]))})}))
+    {:db          (assoc db :loading true)
+     :http-xhrio  {:method          :get
+                   :uri             (str "/api/fragen-from-test/" test-id)
+                   :timeout         8000
+                   :response-format (ajax/text-response-format)
+                   :on-success      [:update-fragen-for-test]}}))
 
 
 (rf/reg-event-fx
   :retrieve-antwort-for-this-user-frage
   (fn [{:keys [db]} [_ [user-id frage-id]]]
-    {:db db
-     :dispatch (GET (str "/api/antworten-from-user-frage/" user-id "/" frage-id)
-                    {:handler (fn [resp]
-                                (rf/dispatch [:update-antwort-for-this-user-frage
-                                              [user-id frage-id (edn/read-string (str "[" resp "]"))]]))})}))
+    {:db          (assoc db :loading true)
+     :http-xhrio  {:method          :get
+                   :uri             (str "/api/antworten-from-user-frage/" user-id "/" frage-id)
+                   :timeout         8000
+                   :response-format (ajax/text-response-format)
+                   :on-success      [:update-antwort-for-this-user-frage]}}))
 
 
 (rf/reg-event-db
@@ -76,23 +76,35 @@
 
 (rf/reg-event-db
   :update-tests-for-this-kurs
-  (fn [db [_ [kurs-id tests]]]
-    (assoc db [:tests-by-kurs kurs-id] tests)))
+  (fn [db [_ kurs-tests]]
+    (let [{kurs-id :kurs-id t :tests} (edn/read-string kurs-tests)]
+      (-> db
+          (assoc :loading false)
+          (assoc [:tests-by-kurs kurs-id] t)))))
 
 
 (rf/reg-event-db
   :update-fach-for-this-kurs
-  (fn [db [_ [kurs-id fach]]]
-    (assoc db [:fach-by-kurs kurs-id] fach)))
+  (fn [db [_ kurs-fach]]
+    (let [{kurs-id :kurs-id [f] :fach} (edn/read-string kurs-fach)]
+      (-> db
+          (assoc :loading false)
+          (assoc [:fach-by-kurs kurs-id] f)))))
 
 
 (rf/reg-event-db
   :update-fragen-for-test
-  (fn [db [_ [test-id fragen]]]
-    (assoc db [:fragen-by-test test-id] fragen)))
+  (fn [db [_ test-fragen]]
+    (let [{test-id :test-id f :fragen} (edn/read-string test-fragen)]
+      (-> db
+          (assoc :loading false)
+          (assoc [:fragen-by-test test-id] f)))))
 
 
 (rf/reg-event-db
   :update-antwort-for-this-user-frage
-  (fn [db [_ [user-id frage-id antworten]]]
-    (assoc db [:antwort-by-user-frage user-id frage-id] antworten)))
+  (fn [db [_ user-frage-antworten]]
+    (let [{user-id :user-id frage-id :frage-id a :antworten} (edn/read-string user-frage-antworten)]
+      (-> db
+          (assoc :loading false)
+          (assoc [:antwort-by-user-frage user-id frage-id] a)))))
