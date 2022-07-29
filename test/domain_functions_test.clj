f43c81f1eee6600305f48480ebe3d8e60bf20821
Lian Remme <lirem101@hhu.de>
2022-07-30 00:32:34 +0800

    Feat: No way to check tests-for-studierenden-uebersicht
    Implemented test-tests-for-studierenden-uebersicht

--- test/domain_functions_test.clj
+++ test/domain_functions_test.clj
@@ -1,5 +1,6 @@
 (ns domain-functions-test
-  {:clj-kondo/config '{:linters {:unresolved-symbol {:exclude (test-fach-for-studierenden-uebersicht test-calc-points)}}}}
+  {:clj-kondo/config '{:linters {:unresolved-symbol
+                                 {:exclude (test-fach-for-studierenden-uebersicht test-calc-points test-tests-for-studierenden-uebersicht)}}}}
   (:require
     [clojure.test.check.clojure-test :refer [defspec]]
     [clojure.test.check.generators :as gen]
@@ -20,6 +21,7 @@
            (= id id-got)
            (= titel titel-got)))))
 
+
 (defspec test-calc-points 10
   (prop/for-all
     [max-punkte (gen/not-empty (gen/list gen/nat))
@@ -33,3 +35,23 @@
           [_ total-points _ reached-points] (df/calc-points nil nil fragen-fct antworten-fct)]
       (and (= summed-max-punkte total-points)
            (= summed-erreichte-punkte reached-points)))))
+
+
+(defspec test-tests-for-studierenden-uebersicht 10
+  (prop/for-all
+    [max-punkte (gen/not-empty (gen/list gen/nat))
+     erreichte-punkte (gen/list gen/nat)
+     test-ids (gen/not-empty (gen/list gen/nat))]
+    (let [fragen (map #(assoc {:frage/id 0 :frage/typ :free-text :frage/frage-text "Text" :frage/loesung "Loesung"} :frage/punkte %) max-punkte)
+          antworten (map #(assoc {:antwort/id 0 :antwort/user [:user/id 0] :antwort/frage [:frage/id 0] :antwort/antwort-text "Text"} :antwort/punkte %) erreichte-punkte)
+          fragen-fct (fn [_id] fragen)
+          antworten-fct (fn [_fid _uid] antworten)
+          summed-max-punkte (apply + max-punkte)
+          summed-erreichte-punkte (* (count max-punkte) (apply max 0 erreichte-punkte))
+          tests (map #(assoc {:test/name "name" :test/fragen []} :test/id %) test-ids)
+          tests-fct (fn [_id] tests)
+          result (df/tests-for-studierenden-uebersicht nil nil tests-fct fragen-fct antworten-fct)]
+      (and (= #{:test/id :test/name :test/max-punkte :test/erreichte-punkte} (into #{} (keys (first result))))
+           (= (sort test-ids) (sort (map :test/id result)))
+           (= summed-max-punkte (:test/max-punkte (first result)))
+           (= summed-erreichte-punkte (:test/erreichte-punkte (first result)))))))
