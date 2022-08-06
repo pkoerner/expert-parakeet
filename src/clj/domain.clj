751b3959f072171b37f79c2c315cf228d8a8d3f5
Lian Remme <lirem101@hhu.de>
2022-08-06 12:38:09 +0800

    Feat: Function missing that got data for korrektur-uebersicht
    Implemented sortierte-antworten-von-freitext-fragen

--- src/clj/domain.clj
+++ src/clj/domain.clj
@@ -35,3 +35,29 @@
          (update kurs :kurs/tests (partial map (partial test-punkte test->antwort))))
        kurse))
 
+
+;; changes a map that has a coll in it (as var) to a coll of maps
+;; all maps have the keys and vals of the outer map and the keys and vals of one inner map
+(defn unpack-map-in-map
+  [key-of-coll input-map]
+  (let [map-without-coll (dissoc input-map key-of-coll)]
+    (map
+      #(merge map-without-coll %)
+      (key-of-coll input-map))))
+
+
+(defn freitext-fragen
+  [kurse-mit-inneren-tests]
+  (let [tests-mit-inneren-fragen (flatten (map (partial unpack-map-in-map :kurs/tests) kurse-mit-inneren-tests))
+        fragen-mit-innerem-fach (flatten (map (partial unpack-map-in-map :test/fragen) tests-mit-inneren-fragen))
+        fragen (map #(dissoc (assoc % :fach/fachtitel (:fach/fachtitel (:kurs/fach %))) :kurs/fach) fragen-mit-innerem-fach)
+        nur-freitext-fragen (filter #(= :frage.typ/text (:frage/typ %)) fragen)]
+    nur-freitext-fragen))
+
+
+(defn sortierte-antworten-von-freitext-fragen
+  [fct-antworten-von-frage kurse-mit-inneren-tests]
+  (let [freitext-fragen (freitext-fragen kurse-mit-inneren-tests)
+        freitext-fragen-mit-inneren-antworten (map #(assoc % :frage/antworten (fct-antworten-von-frage (:frage/id %))) freitext-fragen)
+        antworten (flatten (map (partial unpack-map-in-map :frage/antworten) freitext-fragen-mit-inneren-antworten))]
+    (sort-by :antwort/timestamp antworten)))
