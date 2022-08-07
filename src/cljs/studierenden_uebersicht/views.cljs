ab4a0402063ca4d58e82c65747af749c8ce98616
Lian Remme <lirem101@hhu.de>
2022-08-07 13:24:24 +0800

    Fix: Studi Overview used recom
    Changed to usage of hiccup

--- src/cljs/studierenden_uebersicht/views.cljs
+++ src/cljs/studierenden_uebersicht/views.cljs
@@ -1,7 +1,6 @@
 (ns studierenden-uebersicht.views
   (:require
     [day8.re-frame.http-fx]
-    [re-com.core :refer [at button h-box v-box title]]
     [re-frame.core :as rf]
     [studierenden-uebersicht.events]
     [studierenden-uebersicht.sub]))
@@ -11,14 +10,12 @@
   []
   (let [user @(rf/subscribe [:user])]
     [:div
-     [h-box
-      :gap      "10px"
-      :children [[:div "Logged in as: " user]
-                 [button
-                  :src (at)
-                  :class "button-primary"
-                  ;; :on-click #(rf/dispatch [:logout])
-                  :label "Logout"]]]]))
+     [:p (str "Logged in as: " user)
+      [:input
+       {:type  "button"
+        :value "Logout"
+        ;; :on-click #(rf/dispatch [:logout])
+        }]]]))
 
 
 (defn show-test
@@ -26,27 +23,23 @@
     name :test/name
     max-punkte :test/max-punkte
     erreichte-punkte :test/erreichte-punkte}]
-  [button
-   :label (str name " - Bisher erreichte Punkte: " erreichte-punkte " von " max-punkte)
-   :on-click #(rf/dispatch [:router/push-state {:name :router/test
-                                                :path-params {:id id}}])])
+  [:p
+   [:input
+    {:type "button"
+     :value (str name " - Bisher erreichte Punkte: " erreichte-punkte " von " max-punkte)
+     :on-click #(rf/dispatch [:router/push-state {:name :router/test
+                                                  :path-params {:id id}}])}]])
 
 
 (defn show-kurs
-  [{id :kurs/id
+  [{_id :kurs/id
     jahr :kurs/jahr
     semester :kurs/semester
     fach :kurs/fach
     tests :kurs/tests}]
-  [v-box
-   :src (at)
-   :attr {:key id}
-   :gap "5px"
-   :children
-   [[title
-     :label (str (:fach/fachtitel fach) " - " semester " - " jahr ":")
-     :level :level2]
-    (map show-test tests)]])
+  [:div
+   [:h2 (str (:fach/fachtitel fach) " - " semester " - " jahr ":")]
+   (map show-test tests)])
 
 
 (defn show-kurse-and-tests
