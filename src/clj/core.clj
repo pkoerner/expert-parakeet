a888f27f137747bde6380732de5a828856672696
Tim Matussek <tim.matussek@outlook.de>
2023-07-01 12:20:33 +0200

    fixed formatting

--- src/clj/core.clj
+++ src/clj/core.clj
@@ -1,22 +1,27 @@
 (ns core
-  (:require [auth :refer [wrap-authentication]]
-            [compojure.core :refer [defroutes GET]]
-            [compojure.route :as route]
-            [domain]
-            [hiccup2.core :as h]
-            [ring.adapter.jetty :refer [run-jetty]]
-            [ring.middleware.defaults :refer [site-defaults wrap-defaults]]
-            [ring.middleware.reload :refer [wrap-reload]]
-            [ring.util.response :refer [header response]]))
+  (:require
+    [auth :refer [wrap-authentication]]
+    [compojure.core :refer [defroutes GET]]
+    [compojure.route :as route]
+    [domain]
+    [hiccup2.core :as h]
+    [ring.adapter.jetty :refer [run-jetty]]
+    [ring.middleware.defaults :refer [site-defaults wrap-defaults]]
+    [ring.middleware.reload :refer [wrap-reload]]
+    [ring.util.response :refer [header response]]))
 
-(defn html-response [html] (-> html h/html str response (header "Content-Type" "text/html; charset=utf-8")))
+
+(defn html-response
+  [html]
+  (-> html h/html str response (header "Content-Type" "text/html; charset=utf-8")))
+
 
 ;; all routes that dont need authentication go here
 (defroutes public-routes
   (GET "/" req (html-response
-                (if (auth/is-logged-in req)
-                  [:p (str "Hello, " (str (get-in req [:session :user :id])))] 
-                  [:a {:href "/oauth2/github"} "Login"])))) ; TODO remove route, just an example to show login working
+                 (if (auth/is-logged-in req)
+                   [:p (str "Hello, " (str (get-in req [:session :user :id])))]
+                   [:a {:href "/oauth2/github"} "Login"])))) ; TODO remove route, just an example to show login working
 
 
 ;; all routes that require authentication go here
@@ -28,6 +33,7 @@
 (defroutes combined-routes
   public-routes
   (wrap-authentication private-routes))
+
 
 ;; oauth2 middleware callback requires cookie setting same site to be lax, see: https://github.com/weavejester/ring-oauth2
 (def app (-> combined-routes (wrap-defaults (-> site-defaults (assoc-in [:session :cookie-attrs :same-site] :lax)))))
