dedb61976f3802ad1e73dac0f0a5ab9d9f7bc824
MarkusBott <markus.bott@hhu.de>
2023-07-05 16:42:43 +0200

    change naming corrected->submitted and created functions which creates the hole overview

--- src/clj/views/student_overview.clj
+++ src/clj/views/student_overview.clj
@@ -6,37 +6,45 @@
 ;;   Questions-sets to do:
 ;;     - question-set-3 | (deadline) | "click here to begin"
 ;;     - question-set-4 | (deadline) | "click here to begin"
-;;   Corrected question-sets:
+;;   Submitted question-sets:
 ;;     - question-set-1 | 8/10 Points | passed/failed-checkbox | "see correction"
 ;;     - question-set-2 | 2/10 Points | passed/failed-checkbox | "see correction"
 ;;
 ;; ("" means a hyperlink-button)
-(defn body-for-overview
+(defn question-sets-for-courses
   "Creates a list of the course-iterations of the student.
    Each of the course-iterations contains two list:
    1) question-sets to do
-   2) corrected question-sets"
+   2) submitted question-sets"
   [courses-with-question-sets]
   [:ul 
-   [:li [:h3 "Courses:"]]
+   [:li [:h2 "Courses:"]]
    (doseq [[name tests] courses-with-question-sets]
-     (let [[corrected uncorrected] (group-by is-corrected? tests)]
+     (let [[submitted to-do] (group-by is-submitted? tests)]
        [:li 
-        [:h4 name] 
+        [:h3 name] 
         [:table
          [:tbody
           [:tr [:th {:colspan "3"} "Question-sets to do:"]]
-          (for [question-set uncorrected]
+          (for [question-set to-do]
             [:tr
              [:td (:question-set/name question-set)]
              [:td (:question-set/end question-set)]
              [:td [:button {:onclick "/"} "begin"]]])]]
         [:table
          [:tbody
-          [:tr [:th {:colspan "4"} "Corrected question-sets"]]
-          (for [question-set corrected]
+          [:tr [:th {:colspan "4"} "Submitted question-sets"]]
+          (for [question-set submitted]
             [:tr
              [:td (:question-set/name question-set)]
              [:td "points"]
              [:td "passed/failed-checkbox"]
              [:td [:button {:onclick "/"} "see correction"]]])]]]))])
+
+(defn overview
+  [user-id courses-with-question-sets]
+  [:div
+   [:h1 "Overview"]
+   (question-sets-for-courses courses-with-question-sets)
+   ]
+  )
