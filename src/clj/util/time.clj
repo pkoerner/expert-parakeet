44cecb5fdc38b9b90034284492875cd500e4de9f
matlorr <matvey.lorkish@outlook.de>
2023-07-06 21:48:28 +0200

    fixed formatting

--- src/clj/util/time.clj
+++ src/clj/util/time.clj
@@ -9,6 +9,7 @@
   [{:keys [year month day hour minute]}]
   (java.util.Date. (- year 1900) month day hour minute))
 
+
 (defn is-date-object?
   "Checks if provided argument
    is an instance of the java.util.Date
@@ -16,12 +17,13 @@
   [timestamp]
   (instance? java.util.Date timestamp))
 
+
 (defn start-before-end?
   "Returns true if bot arguments
    are java.util.Date instances
    and the start date is earlier
    than the end date."
   [start end]
-  (and (is-date-object? start) 
-       (is-date-object? end) 
+  (and (is-date-object? start)
+       (is-date-object? end)
        (= -1 (compare start end))))
