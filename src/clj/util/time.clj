0fe776cc129910401956fa9bc675f122d1af563d
matlorr <matvey.lorkish@outlook.de>
2023-07-06 21:37:13 +0200

    Provided new functions for validating and comparing Java.util.Date objects

--- src/clj/util/time.clj
+++ src/clj/util/time.clj
@@ -8,3 +8,20 @@
    :minutes."
   [{:keys [year month day hour minute]}]
   (java.util.Date. (- year 1900) month day hour minute))
+
+(defn is-date-object?
+  "Checks if provided argument
+   is an instance of the java.util.Date
+   class."
+  [timestamp]
+  (instance? java.util.Date timestamp))
+
+(defn start-before-end?
+  "Returns true if bot arguments
+   are java.util.Date instances
+   and the start date is earlier
+   than the end date."
+  [start end]
+  (and (is-date-object? start) 
+       (is-date-object? end) 
+       (= -1 (compare start end))))
