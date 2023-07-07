98a3ffa111eca065dd12e5653e497ff470e7a5c9
jaess105 <jaess105@uni-duesseldorf.de>
2023-07-07 06:51:38 +0200

    added a view and actions when posting a course-iteration form

--- src/clj/views/course_iteration/create_course_iteration_view.clj
+++ src/clj/views/course_iteration/create_course_iteration_view.clj
@@ -57,3 +57,63 @@
     (if (nil? courses)
       no-courses
       (course-iteration-form courses question-sets post-destination))))
+
+
+(defn- validate-course-iteration
+  [course-id year semester question-set-ids]
+  (let [error-map {:course/id "Der ausgewählte Kurs war inkorrekt!"
+                   :course-iteration/year "Das ausgewählte Jahr war inkorrekt!"
+                   :course-iteration/semester "Das ausgewählte Semester war inkorrekt!"
+                   :question-set/id "Das ausgewählte question-set-war nicht korrekt!"}]
+
+    (reduce (fn [error-col [spec val]]
+              (if (s/valid? spec val)
+                error-col
+                (conj error-col (error-map spec))))
+            []
+            [[:course/id course-id]
+             [:course-iteration/year year]
+             [:course-iteration/semester semester]
+             [(s/coll-of :question-set/id) question-set-ids]])))
+
+(defn- error-view
+  [errors]
+  (h/html
+   [:div
+    [:p "Es gab folgende Fehler in der Eingabe:"]
+    [:br]
+    [:ul (for [error errors]
+           [:li error])]]
+   [:div
+    [:p "Bitte gebe diesmal Korrekte Werete an!"]]))
+
+(defn- add-to-db-and-get-succsess-msg
+  [course-id year semester question-set-ids db-add-fun]
+  (let [db-result (db-add-fun
+                   course-id year semester question-set-ids)]
+    [:div
+     [:p "Der Kurs für das " (:course-iteration/semester db-result)
+      " im Jahr " (:course-iteration/year db-result)
+      " wurde Erfolgreich für das Fach erstellt!\n"]]))
+
+(defn submit-create-course-iteration-mockable!
+  [request db-add-fun]
+  (let [form-data (-> request (:multipart-params) (dissoc :__anti-forgery-token))
+        course-id (form-data "course-id")
+        year (read-string (form-data "year"))
+        semester (form-data "semester")
+        question-set-ids (let [ids-or-id (form-data "question-set-ids")]
+                           (cond (coll? ids-or-id) ids-or-id
+                                 (nil? ids-or-id) []
+                                 :else [ids-or-id]))
+        validation-errors (validate-course-iteration course-id year semester question-set-ids)]
+
+    (if (empty? validation-errors)
+      (add-to-db-and-get-succsess-msg course-id year semester question-set-ids db-add-fun)
+      (error-view validation-errors))))
+
+(defn submit-create-course-iteration!
+  [request]
+  (submit-create-course-iteration-mockable!
+   request
+   db/add-course-iteration-with-question-sets!))
