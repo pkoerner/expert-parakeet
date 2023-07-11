c8b2f8dcee71c4adc5e041f9674f11b696fb9d73
jaess105 <jaess105@uni-duesseldorf.de>
2023-07-11 21:40:16 +0200

    fixed formatting

--- src/clj/views/course_iteration/create_course_iteration_view.clj
+++ src/clj/views/course_iteration/create_course_iteration_view.clj
@@ -1,10 +1,10 @@
 (ns views.course-iteration.create-course-iteration-view
   (:require
-   [clojure.spec.alpha :as s]
-   [clojure.string :as string]
-   [hiccup.form :as hform]
-   [hiccup2.core :as h]
-   [ring.util.anti-forgery :refer [anti-forgery-field]]))
+    [clojure.spec.alpha :as s]
+    [clojure.string :as string]
+    [hiccup.form :as hform]
+    [hiccup2.core :as h]
+    [ring.util.anti-forgery :refer [anti-forgery-field]]))
 
 
 (def create-course-iteration-errors
@@ -31,18 +31,18 @@
 
 
 (s/fdef course-iteration-form
-  :args (s/cat :courses (s/coll-of (s/keys :req [:course/id :course/course-name]) :distinct true)
-               :question-sets (s/coll-of (s/keys :req [:question-set/id :question-set/name]) :distinct true)
-               :post-destination :general/non-blank-string
-               :kwargs (s/? (s/map-of (set (keys create-course-iteration-errors-spec))
-                                      string?)))
-  :ret #(instance? hiccup.util.RawString %)
-  :fn (fn [spec-map]
-        (let [{{:keys [courses question-sets post-destination]} :args
-               ret :ret} spec-map]
-          (s/and (every? #(string/includes? (str ret) (:course/course-name %)) courses)
-                 (every? #(string/includes? (str ret) (:question-set/name %)) question-sets)
-                 #(string/includes? ret post-destination)))))
+        :args (s/cat :courses (s/coll-of (s/keys :req [:course/id :course/course-name]) :distinct true)
+                     :question-sets (s/coll-of (s/keys :req [:question-set/id :question-set/name]) :distinct true)
+                     :post-destination :general/non-blank-string
+                     :kwargs (s/? (s/map-of (set (keys create-course-iteration-errors-spec))
+                                            string?)))
+        :ret #(instance? hiccup.util.RawString %)
+        :fn (fn [spec-map]
+              (let [{{:keys [courses question-sets post-destination]} :args
+                     ret :ret} spec-map]
+                (s/and (every? #(string/includes? (str ret) (:course/course-name %)) courses)
+                       (every? #(string/includes? (str ret) (:question-set/name %)) question-sets)
+                       #(string/includes? ret post-destination)))))
 
 
 (defn course-iteration-form
@@ -58,62 +58,62 @@
             [course]
             [(:course/course-name course) (:course/id course)])]
     (h/html
-     (hform/form-to
-      {:enctype "multipart/form-data"}
-      [:post post-destination]
+      (hform/form-to
+        {:enctype "multipart/form-data"}
+        [:post post-destination]
 
-      [:div
-       (optional-error-display :course-error errors)
-       [:label {:for "courses"} "Fach auswahl:"] [:br]
-       [:select#courses {:name "course-id"}
-        (hform/select-options (mapv course-to-option courses))]]
+        [:div
+         (optional-error-display :course-error errors)
+         [:label {:for "courses"} "Fach auswahl:"] [:br]
+         [:select#courses {:name "course-id"}
+          (hform/select-options (mapv course-to-option courses))]]
 
-      [:div
-       (optional-error-display :year-error errors)
-       [:label {:for "year"} "Jahr"] [:br]
-       [:input#year {:name "year"
-                     :type "number"
-                     :min "1900"
-                     :max "2099"
-                     :step "1"
-                     :value "2024"}]]
+        [:div
+         (optional-error-display :year-error errors)
+         [:label {:for "year"} "Jahr"] [:br]
+         [:input#year {:name "year"
+                       :type "number"
+                       :min "1900"
+                       :max "2099"
+                       :step "1"
+                       :value "2024"}]]
 
-      [:div
-       (optional-error-display :semester-error errors)
-       [:label {:for "semester"} "Semester"] [:br]
-       [:select#semester {:name "semester"}
-        (hform/select-options [["Sommer" "SoSe"]
-                               ["Winter" "WiSe"]])]]
+        [:div
+         (optional-error-display :semester-error errors)
+         [:label {:for "semester"} "Semester"] [:br]
+         [:select#semester {:name "semester"}
+          (hform/select-options [["Sommer" "SoSe"]
+                                 ["Winter" "WiSe"]])]]
 
-      [:div
-       (optional-error-display :question-set-error errors)
-       [:label {:for "question-sets"} "Tests"] [:br]
-       [:ul
-        (for [question-set question-sets]
-          [:li [:input {:name "question-set-ids"
-                        :type "checkbox"
-                        :value (:question-set/id question-set)}
-                (:question-set/name question-set)]])]]
+        [:div
+         (optional-error-display :question-set-error errors)
+         [:label {:for "question-sets"} "Tests"] [:br]
+         [:ul
+          (for [question-set question-sets]
+            [:li [:input {:name "question-set-ids"
+                          :type "checkbox"
+                          :value (:question-set/id question-set)}
+                  (:question-set/name question-set)]])]]
 
 
-      (h/raw (anti-forgery-field))
-      (hform/submit-button "submit")))))
+        (h/raw (anti-forgery-field))
+        (hform/submit-button "submit")))))
 
 
 (s/fdef submit-success-view
-  :args (s/cat :semester :course-iteration/semester
-               :year :course-iteration/semester)
-  :ret (s/and #(string/includes? % "erfolg")
-              #(instance? hiccup.util.RawString %)))
+        :args (s/cat :semester :course-iteration/semester
+                     :year :course-iteration/semester)
+        :ret (s/and #(string/includes? % "erfolg")
+                    #(instance? hiccup.util.RawString %)))
 
 
 (defn submit-success-view
   "Returns a div with a success messag displaying the `semester` and the `year` of the created course-iteration."
   [semester year]
   (h/html
-   [:div
-    [:p "Der Kurs für das " semester
-     " im Jahr " year
-     " wurde erfolgreich für das Fach erstellt!\n"]]))
+    [:div
+     [:p "Der Kurs für das " semester
+      " im Jahr " year
+      " wurde erfolgreich für das Fach erstellt!\n"]]))
 
 
