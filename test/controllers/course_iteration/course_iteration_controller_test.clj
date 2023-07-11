732168b016f7adb6a60b56f5052b537f728afffa
jaess105 <jaess105@uni-duesseldorf.de>
2023-07-11 21:06:59 +0200

    updated the tests for the post request to not use mockable

--- test/controllers/course_iteration/course_iteration_controller_test.clj
+++ test/controllers/course_iteration/course_iteration_controller_test.clj
@@ -29,7 +29,7 @@
                              (string/includes? res post-destination))))))
 
 
-(deftest test-submit-create-course-iteration-mockable
+(deftest test-submit-create-course-iteration!
   (testing "Test that the db-add-function get's called with the correct values with different parameters."
     (let [test-request {:__anti-forgery-token ""
                         :multipart-params {"course-id" "123"
@@ -40,21 +40,23 @@
                             (t/is (s/valid? :course/id course-id))
                             (t/is (s/valid? :course-iteration/year year))
                             (t/is (s/valid? :course-iteration/semester semester))
-                            (t/is (s/valid? (s/coll-of :question-set/id) question-set-ids)))]
+                            (t/is (s/valid? (s/coll-of :question-set/id) question-set-ids)))
+          redirect-uri "S"]
       (doseq [course-id ["1" "50009"]
               year ["2023" "2024"]
               semester ["WiSe" "SoSe"]
               question-set-ids [[] ["1" "2" "3"]]]
-        (submit-create-course-iteration-mockable
+        (submit-create-course-iteration!
           (-> test-request
               (assoc-in [:multipart-params "course-id"] course-id)
               (assoc-in [:multipart-params "year"] year)
               (assoc-in [:multipart-params "semester"] semester)
               (assoc-in [:multipart-params "question-set-ids"] question-set-ids))
-          db-add-fun-stub))))
+          redirect-uri
+          :db-add-fun db-add-fun-stub))))
 
   (testing "Test that the db-add-function is not called when the parameters are invalid. 
-            And that the correct error message is displayed."
+            And that the correct error message is send with the redirect."
     (let [test-request {:__anti-forgery-token ""
                         :multipart-params {"course-id" "123"
                                            "year" "2023"
@@ -62,14 +64,18 @@
                                            "question-set-ids" ["1"]}}
           db-add-fun-stub (fn [_course-id _year _semester _question-set-ids]
                             (t/is false "The function to create a course-itration was called but shouldn't be!"))
+          redirect-uri "S"
           wrong-course-id ""
           wrong-year "-2023"
           wrong-semester "Sommer"
           wrong-question-set-ids [""]]
 
       (t/are [request expected-error]
-             (string/includes? (submit-create-course-iteration-mockable request db-add-fun-stub)
-                               expected-error)
+             (let [response (submit-create-course-iteration! request redirect-uri :db-add-fun db-add-fun-stub)
+                   response-status (:status response)
+                   response-url (get-in response [:headers "Location"])]
+               (and (= 302 response-status)
+                    (string/includes? response-url (form-encode expected-error))))
         (assoc-in test-request [:multipart-params "course-id"] wrong-course-id)
         "Der ausgewählte Kurs war inkorrekt!"
 
@@ -81,7 +87,4 @@
 
         (assoc-in test-request [:multipart-params "question-set-ids"] wrong-question-set-ids)
         "Das ausgewählte question-set-war nicht korrekt!"))))
-
-
-(t/run-test test-submit-create-course-iteration-mockable)
 
