1fc6cb2c0a0f4cead9646c0e4a5589f560f1cb78
matlorr <matvey.lorkish@outlook.de>
2023-07-14 10:31:10 +0200

    Created question view

---
+++ src/clj/views/question.clj
@@ -0,0 +1,19 @@
+(ns views.question
+  (:require 
+   [db]
+   [services.question-service :refer [extract-question-id-from-question
+                                              extract-question-statement-from-question
+                                              dispatch-question-type]]
+   [hiccup.form :refer [form-to
+                        submit-button]]))
+
+
+(defn question-form
+  [question-id]
+  (form-to [:put (str "/answer/" question-id "/" (rand-int 30))]
+                (let [question (db/get-question-by-id question-id)]
+                  [:div [:p
+                         {:id (extract-question-id-from-question question)}
+                         (extract-question-statement-from-question question)]
+                  (dispatch-question-type question)])
+                (submit-button "submit")))
