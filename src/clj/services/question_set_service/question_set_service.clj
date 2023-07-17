f62708bbcd8d237eb6038474ca46866b251a567f
jaess105 <jaess105@uni-duesseldorf.de>
2023-07-17 14:17:56 +0200

    Refactored `QuestionSetService` to allow specing of functions

--- src/clj/services/question_set_service/question_set_service.clj
+++ src/clj/services/question_set_service/question_set_service.clj
@@ -1,13 +1,25 @@
 (ns services.question-set-service.question-set-service
   (:require
+    [clojure.spec.alpha :as s]
     [db]
     [services.question-set-service.p-question-set-service :refer [PQuestionSetService]]))
 
 
 ;; todo replace all direct db calls and inject repositories
 (deftype QuestionSetService
-  []
+  [])
 
+
+(s/fdef get-all-question-sets
+        :args (s/cat :self #(satisfies? PQuestionSetService %))
+        :ret (s/coll-of (s/keys :req [:question-set/id])))
+
+
+(defn get-all-question-sets
+  [_]
+  (db/get-all-question-sets))
+
+
+(extend QuestionSetService
   PQuestionSetService
-
-  (get-all-question-sets [_] (db/get-all-question-sets)))
+  {:get-all-question-sets get-all-question-sets})
