925dc4e27f05212e070dd4812fd69145f17c9b4f
matlorr <matvey.lorkish@outlook.de>
2023-07-17 16:46:51 +0200

    Added error view to question-set view and extracted logic to controller

--- src/clj/views/question_set.clj
+++ src/clj/views/question_set.clj
@@ -1,19 +1,25 @@
 (ns views.question-set
   (:require
-    [application-services.question-set-service :as serv]
     [db]))
 
+(def question-set-errors
+  "Possible errors that can occur when trying
+   to access a question-set."
+  {:not-assigned-to-question-set "Sie haben keinen Zugriff auf diesen Test!"})
 
 (defn question-set-form
-  [question-set-id]
-  [:h1 (serv/extract-question-set-name question-set-id)]
+  [question-set]
+  [:h1 (:question-set/name question-set)]
   [:table
    [:tr
     [:th "Question"]
     [:th "Points"]]
-   (for [question (serv/extract-question-from-question-set question-set-id)]
+   (for [question (:question-set/questions question-set)]
      [:tr
       [:td
-       [:a {:href (str "/question/" (serv/extract-question-id-from-question question))}
-        (serv/extract-question-statement-from-question question)]]
-      [:td (serv/extract-question-points-from-question question)]])])
+       [:a {:href (str "/question/" (:question/id question))}
+        (:question/question-statement question)]]
+      [:td (:question/points question)]])])
+
+(defn no-assignement-form [permission-error]
+  [:p (str "ERROR:" (:not-assigned-to-question-set permission-error))])
