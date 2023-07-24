0fd814df98e913c904ced7052a7693361a149802
matlorr <matvey.lorkish@outlook.de>
2023-07-24 15:08:47 +0200

    Changed formatting

--- src/clj/controller/question_set/question_set_controller.clj
+++ src/clj/controller/question_set/question_set_controller.clj
@@ -1,19 +1,21 @@
 (ns controller.question-set.question-set-controller
-   (:require
+  (:require
     [db]
     [views.question-set :as view]))
 
-(defn- extract-question-sets-of-user 
+
+(defn- extract-question-sets-of-user
   [user-id]
   (map
-   #(-> % :course-iteration/question-sets)
-   (db/get-course-iterations-of-student user-id)))
+    #(-> % :course-iteration/question-sets)
+    (db/get-course-iterations-of-student user-id)))
+
 
 (defn- extract-question-set-ids-of-user
   [user-id]
   (apply concat (map
-                 #(map :question-set/id %)
-                 (extract-question-sets-of-user user-id))))
+                  #(map :question-set/id %)
+                  (extract-question-sets-of-user user-id))))
 
 
 (defn- validate-user-for-question-set
@@ -28,17 +30,18 @@
       (dissoc view/question-set-errors :not-assigned-to-question-set)
       error-map)))
 
+
 (defn question-set-get
   "Returns a form for a question-set
    if the current user is assigned to
    this question-set. If not an error
    view will be displayed."
   [req & {:keys [get-question-set-fun]
-        :or {get-question-set-fun db/get-question-set-by-id}}]
-  (let [question-set 
+          :or {get-question-set-fun db/get-question-set-by-id}}]
+  (let [question-set
         (get-question-set-fun (-> req :route-params :id))
         permission-error
-        (validate-user-for-question-set (str(-> req :session :user :id)) (-> req :route-params :id))]
+        (validate-user-for-question-set (str (-> req :session :user :id)) (-> req :route-params :id))]
     (if (empty? permission-error)
       (view/question-set-form question-set)
       (view/no-assignement-form permission-error))))
