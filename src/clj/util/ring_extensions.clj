bb2899fbbcd8a5b8646fb47a3542b42350a067a7
jaess105 <jaess105@uni-duesseldorf.de>
2023-07-25 20:39:49 +0200

    added a bootstrap include for html-response.
    This can be removed later but enables basic styling with bootstrap for now

--- src/clj/util/ring_extensions.clj
+++ src/clj/util/ring_extensions.clj
@@ -1,9 +1,15 @@
 (ns util.ring-extensions
   (:require
-    [hiccup2.core :as h]
-    [ring.util.response :refer [header response]]))
+   [hiccup2.core :as h]
+   [ring.util.response :refer [header response]]))
+
+(def ^:private bootstrap-include
+  [:link {:rel "stylesheet"
+          :href "https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
+          :integrity "sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+          :crossorigin "anonymous"}])
 
 
 (defn html-response
   [html]
-  (-> html h/html str response (header "Content-Type" "text/html; charset=utf-8")))
+  (-> (h/html bootstrap-include html) str response (header "Content-Type" "text/html; charset=utf-8")))
