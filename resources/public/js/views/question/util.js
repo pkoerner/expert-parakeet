67e18042ddd7a5f60442890301567a1fd56f1422
jaess105 <jaess105@uni-duesseldorf.de>
2023-07-25 20:39:13 +0200

    create button function cann add css classes

--- resources/public/js/views/question/util.js
+++ resources/public/js/views/question/util.js
@@ -12,14 +12,17 @@ function createNodeFromText(htmlText) {
 /**
  * Creates a button dom element with type button. The displayed field of the button is text
  * and onclick is set to the passed in onClick function.
+ * Optionally an array of css classes can be passed to the function as well.
  * @param {String} text
  * @param {Function} onClick
+ * @param {Array[String]} classes
  * @returns A button Dom element.
  */
-function createButton(text, onClick) {
+function createButton(text, onClick, classes=[]) {
   const button = document.createElement("button");
   button.type = "button";
   button.innerText = text;
   button.onclick = onClick;
+  classes.forEach(x => button.classList.add(x));
   return button;
 }
