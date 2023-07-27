01b4eece4c55b0146c08ce714a22cdeeeae530ef
jaess105 <jaess105@uni-duesseldorf.de>
2023-07-27 13:05:55 +0200

    added question service protocol

---
+++ src/clj/services/question_service/p_question_service.clj
@@ -0,0 +1,18 @@
+(ns services.question-service.p-question-service)
+
+(defprotocol PQuestionService
+
+  (create-question!
+    [self]
+    "Creates a course-iteration entry in the database and returns a course-iteration map.")
+
+  (validate-question
+    [self
+     question-statement achivable-points type
+     possible-solutions single-choice-solutions multiple-choice-solutions
+     evaluation-criteria
+     categories]
+    "Takes all possible values for a question as arguments and validates them.
+     If there was no parsing/validation error, a valid question map is returned.
+     If there were parsing/validation errors, a map is returned that atleast contains an `:error` key 
+     mapped to a map of keys from the :question namespace to `str` error messages."))
