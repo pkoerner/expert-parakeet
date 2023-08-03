5b36f2a65eb3f4d45ebae8579fc1823ffcc83f0f
matlorr <matvey.lorkish@outlook.de>
2023-08-03 18:11:19 +0200

    Transferred views into subfolders

---
+++ src/clj/views/question_set/question_set_view.clj
@@ -0,0 +1,30 @@
+(ns views.question-set.question-set-view
+  (:require
+    [db]))
+
+
+(def question-set-errors
+  "Possible errors that can occur when trying
+   to access a question-set."
+  #{:not-assigned-to-question-set})
+
+
+(defn question-set-form
+  [question-set]
+  [:h1 (:question-set/name question-set)]
+  [:table
+   [:tr
+    [:th "Question"]
+    [:th "Points"]]
+   (for [question (:question-set/questions question-set)]
+     [:tr
+      [:td
+       [:a {:href (str "/question/" (:question/id question))}
+        (:question/question-statement question)]]
+      [:td (:question/points question)]])])
+
+
+(defn no-assignement-form
+  [permission-error]
+  (when (contains? permission-error :not-assigned-to-question-set)
+    [:p (str "ERROR: Sie haben keinen Zugriff auf diesen Test!")]))
