97e937f8f81f1c02f922e1ec996c996e2d4d29d3
Matvey Lorkish <49522528+matlorr@users.noreply.github.com>
2023-08-04 12:03:14 +0200

    Delete resources/public/js/compiled directory

--- resources/public/js/compiled/cljs-runtime/shadow.module.app.append.js
+++
@@ -1,4 +0,0 @@
-
-shadow.cljs.devtools.client.env.module_loaded('app');
-
-try { core.init(); } catch (e) { console.error("An error occurred when calling (core/init)"); throw(e); }
--- resources/public/js/compiled/cljs-runtime/vars.js
+++
@@ -1,4 +0,0 @@
-goog.provide('vars');
-vars.base_url = "http://localhost:8082/api";
-
-//# sourceMappingURL=vars.js.map
