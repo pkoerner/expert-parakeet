55de35f58c4de7c8ab655542ffb66c9d631cb733
Jakob Scheumann <jakob.scheumann@uni-duesseldorf.de>
2023-08-04 11:00:31 +0200

    Revert "uber-jar had resources at the wrong path"
    This reverts commit 0cd21a6061948dcbac39b0c0983b75d41e810a4a.

--- build.clj
+++ build.clj
@@ -5,7 +5,6 @@
 
 (def lib 'pkoerner/expert-parakeet)
 (def class-dir "target/classes")
-(def resources-dir "target/classes/resources")
 (def basis (b/create-basis {:project "deps.edn"}))
 (def jar-file (format "target/expert-parakeet.jar"))
 (def uber-file (format "target/expert-parakeet-standalone.jar"))
@@ -22,10 +21,8 @@
                 :lib lib
                 :basis basis
                 :src-dirs ["src"]})
-  (b/copy-dir {:src-dirs ["src"]
+  (b/copy-dir {:src-dirs ["src" "resources"]
                :target-dir class-dir})
-  (b/copy-dir {:src-dirs ["resources"]
-               :target-dir resources-dir})
   (b/jar {:class-dir class-dir
           :jar-file jar-file}))
 
@@ -33,10 +30,8 @@
 (defn uber
   [_]
   (clean nil)
-  (b/copy-dir {:src-dirs ["src"]
+  (b/copy-dir {:src-dirs ["src" "resources"]
                :target-dir class-dir})
-  (b/copy-dir {:src-dirs ["resources"]
-               :target-dir resources-dir})
   (b/compile-clj {:basis basis
                   :src-dirs ["src"]
                   :class-dir class-dir})
