3bf4f8e2d5cd69899735812e32d5d2dcadb31873
JanKneiphof <44272737+JanKneiphof@users.noreply.github.com>
2023-08-10 22:42:48 +0200

    add correction service & protocol

---
+++ src/clj/services/correction_service/p_correction_service.clj
@@ -0,0 +1,8 @@
+(ns services.correction-service.p-correction-service)
+
+
+(defprotocol PCorrectionService
+
+  (get-corrections-from-user-as
+    [self user-id role]
+    "Gets the corrections from a single user by their role. (Either :corrector or :student (for now, roles aren't yet implemented to this extent))"))
