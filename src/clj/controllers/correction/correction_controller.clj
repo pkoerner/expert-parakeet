416029b6c34244478e347adc1032f98c1ba3e4ad
JanKneiphof <44272737+JanKneiphof@users.noreply.github.com>
2023-08-10 22:43:39 +0200

    implement correction controller

---
+++ src/clj/controllers/correction/correction_controller.clj
@@ -0,0 +1,36 @@
+(ns controllers.correction.correction-controller
+  (:require
+    [clojure.spec.alpha :as s]
+    [db]
+    [services.correction-service.p-correction-service :refer [PCorrectionService get-corrections-from-user-as]]
+    [views.correction.correction-overview-view :as view]))
+
+
+(def page-size 3)
+
+(def tabs {"corrections" "Korrekturen", "corrected" "Von mir korrigierte Abgaben"})
+
+
+(s/fdef correction-overview-get
+        :args (s/cat :req coll?
+                     :correction-service #(satisfies? PCorrectionService %))
+        :ret #(instance? hiccup.util.RawString %))
+
+
+(defn correction-overview-get
+  [req correction-service]
+  (let [tab-param (get-in req [:params :tab] "corrections")
+        active-tab (if (contains? tabs tab-param) tab-param "corrections")
+        active-page (try (Integer/parseInt (get-in req [:params :page] "1")) (catch Exception _e 1))
+        user-id (-> req :session :user :id)
+        corrections    (case active-tab
+                         "corrections" (get-corrections-from-user-as correction-service user-id :student)
+                         "corrected" (get-corrections-from-user-as correction-service user-id :corrector)
+                         [])
+        page-result (->> corrections
+                         (sort-by #(% :correction/timestamp))
+                         (drop (* page-size (dec active-page)))
+                         (take page-size))
+        number-of-pages (int (Math/ceil (/ (count corrections) page-size)))]
+
+    (view/correction-overview page-result tabs active-tab number-of-pages active-page)))
