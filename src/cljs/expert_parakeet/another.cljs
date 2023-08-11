df6ea7a6979c4e19e99d9066e784036de2608aa5
jaess105 <jaess105@uni-duesseldorf.de>
2023-08-11 11:51:44 +0200

    remove old cljs example files

--- src/cljs/expert_parakeet/another.cljs
+++
@@ -1,9 +0,0 @@
-(ns expert-parakeet.another
-  (:require [goog.dom :as dom]))
-
-(defn on-page-load []
-  (let [elements (dom/getElementsByTagName "body")
-        element (first elements)]
-    (set! (.-textContent element) "Hello there!")))
-
-(on-page-load)
