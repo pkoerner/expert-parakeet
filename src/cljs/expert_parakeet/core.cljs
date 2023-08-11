c911d5674f431af790dd17fd3d47b6009d10f5d4
jaess105 <jaess105@uni-duesseldorf.de>
2023-08-11 11:52:22 +0200

    removed direct call of function and added documentation

--- src/cljs/expert_parakeet/core.cljs
+++ src/cljs/expert_parakeet/core.cljs
@@ -1,9 +1,13 @@
 (ns expert-parakeet.core
   (:require [goog.dom :as dom]))
 
-(defn on-page-load []
+(defn on-page-load 
+  "Example for cljs.
+   For including this in the html, include the resources/public/cljs/main.js file.
+   Afterwards call `goog.require('expert-parakeet.core');` in one script tag.
+   Then call `expert_parakeet.core.on_page_load();` in another script tag."
+  []
   (let [elements (dom/getElementsByTagName "body")
         element (first elements)]
     (set! (.-textContent element) (str (.-textContent element) "Hello from ClojureScript!"))))
 
-(on-page-load)
