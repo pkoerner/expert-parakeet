058eebd2acf5a51c4587a1203e57df07c8b9a856
Taha El Amine Kassabi <taha.kassabi@gmail.com>
2024-08-07 18:39:11 +0200

    Fix - Small Typo

--- src/clj/services/answer_service/p_answer_service.clj
+++ src/clj/services/answer_service/p_answer_service.clj
@@ -5,4 +5,4 @@
 
   (add-user-answer-to-question!
     [self user-id question-id answer-or-solution-ids]
-    "Adds an answer in from of a string or solution-id-vector to the database."))
+    "Adds an answer in form of a string or solution-id-vector to the database."))
