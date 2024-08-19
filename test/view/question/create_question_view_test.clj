a419525f3423d5abb6eacc3b3964eac2da07e210
iTitus <iTitus@users.noreply.github.com>
2024-08-19 11:22:19 +0200

    Remove another accidental change

--- test/view/question/create_question_view_test.clj
+++ test/view/question/create_question_view_test.clj
@@ -26,12 +26,6 @@
       ["cat1" "Cat2" "more" "Categories" "than" "I" "can" "count" "so" "much"]
       "https://some.url"))
 
-  (testing "Test that a text-area is shown in the view, when the question is a free text question."
-    (let [question {:question/type :question.type/free-text
-                    :question/statement "Why 42?"}
-          dispatched-form (create-question-form question "https://some.url")]
-      (t/is (string/includes? dispatched-form "textarea"))))
-
   (testing "Testing that errors are displayed in the form, when errors are passed to the view."
     (let [categories []
           post-destination "https://some.url"
