f94b4bbed5e78f5df88728bc6108bfdea789b9f9
DerHyper <maxrichter15@web.de>
2025-07-26 16:59:07 +0200

    add(test): xss course-iteration-form

--- test/view/xss_test.clj
+++ test/view/xss_test.clj
@@ -3,6 +3,7 @@
    [clojure.string :as string]
    [clojure.test :as t :refer [deftest testing]]
    [views.answer.answer-view :refer [submit-success-view]]
+   [views.course-iteration.create-course-iteration-view :refer [course-iteration-form]]
    [views.course.create-course-view :refer [create-course-form]]
    [views.question.create-question-view :refer [create-question-form]]))
 
@@ -13,4 +14,5 @@
     (t/are [html-output] (not (string/includes? html-output xss-data))
       (create-course-form "https://some.url" :course-data {:name xss-data})
       (create-question-form [] "https://some.url" :question-data {:statement xss-data :criteria xss-data :category xss-data})
-      (submit-success-view xss-data))))
+      (submit-success-view xss-data)
+      (course-iteration-form [{:course/name xss-data}] [{:question-set/name xss-data}] "https://some.url"))))
