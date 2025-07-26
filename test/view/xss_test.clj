a3655c97cb5af7ee7dee47ca0052db6c81c43ee5
DerHyper <maxrichter15@web.de>
2025-07-26 16:26:56 +0200

    refactor(test): xss one test

--- test/view/xss_test.clj
+++ test/view/xss_test.clj
@@ -2,19 +2,15 @@
   (:require
    [clojure.string :as string]
    [clojure.test :as t :refer [deftest testing]]
+   [views.answer.answer-view :refer [submit-success-view]]
    [views.course.create-course-view :refer [create-course-form]]
    [views.question.create-question-view :refer [create-question-form]]))
 
+(def xss-data "<script>alert('XSS')</script>")
+
 (deftest test-create-course-view
-  (testing "Strings containing html-code in the create-course-form should be escaped to prevent XSS."
-    (let [xss-data "<script>alert('XSS')</script>"
-          test-result (create-course-form "https://some.url" :course-data {:name xss-data})]
-      (t/is (not (string/includes? test-result xss-data))))))
-
-
-(deftest test-create-question-form
-  (testing "Strings containing html-code in the create-question-form should be escaped to prevent XSS."
-    (let [xss-data "<script>alert('XSS')</script>"
-          test-result (create-question-form [] "https://some.url" :question-data {:statement xss-data :criteria xss-data :category xss-data})]
-      (println test-result)
-      (t/is (not (string/includes? test-result xss-data)) "XSS payload should not appear unescaped in the any field."))))
+  (testing "Strings containing html-code should be escaped to prevent XSS."
+    (t/are [html-output] (not (string/includes? html-output xss-data))
+      (create-course-form "https://some.url" :course-data {:name xss-data})
+      (create-question-form [] "https://some.url" :question-data {:statement xss-data :criteria xss-data :category xss-data})
+      (submit-success-view xss-data))))
