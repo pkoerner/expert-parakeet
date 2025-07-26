f765ce02f0ebe7538825728c291bda315f319c72
DerHyper <maxrichter15@web.de>
2025-07-26 18:44:00 +0200

    add(test): xss tests Added more tests and added categories in coments

--- test/view/xss_test.clj
+++ test/view/xss_test.clj
@@ -2,17 +2,32 @@
   (:require
    [clojure.string :as string]
    [clojure.test :as t :refer [deftest testing]]
-   [views.answer.answer-view :refer [submit-success-view]]
-   [views.course-iteration.create-course-iteration-view :refer [course-iteration-form]]
-   [views.course.create-course-view :refer [create-course-form]]
-   [views.question.create-question-view :refer [create-question-form]]))
+   [views.answer.answer-view :as av]
+   [views.course-iteration.create-course-iteration-view :as cciv :refer [course-iteration-form]]
+   [views.course.create-course-view :as ccv :refer [create-course-form]]
+   [views.question.create-question-view :refer [create-question-form
+                                                question-success-view]]
+   [views.question.question-view :refer [question-form]]))
 
 (def xss-data "<script>alert('XSS')</script>")
 
-(deftest test-create-course-view
+(def xss-question {:question/type :question.type/free-text
+               :question/statement xss-data 
+               :question/max-points 10
+               :question/categories [xss-data]})
+
+(deftest test-xss
   (testing "Strings containing html-code should be escaped to prevent XSS."
     (t/are [html-output] (not (string/includes? html-output xss-data))
+      ;; Create Course
       (create-course-form "https://some.url" :course-data {:name xss-data})
-      (create-question-form [] "https://some.url" :question-data {:statement xss-data :criteria xss-data :category xss-data})
-      (submit-success-view xss-data)
-      (course-iteration-form [{:course/name xss-data}] [{:question-set/name xss-data}] "https://some.url"))))
+      (ccv/submit-success-view {:course/name xss-data})
+      ;; Create Course Interation
+      (course-iteration-form [{:course/name xss-data}] [{:question-set/name xss-data}] "https://some.url")
+      (cciv/submit-success-view {:course-iteration/semester xss-data, :course-iteration/year xss-data})
+      ;; Create Question
+      (create-question-form [xss-data] "https://some.url" xss-question)
+      (question-success-view xss-question)
+      ;; Answer Question
+      (question-form xss-question "https://some.url")
+      (av/submit-success-view xss-data))))
