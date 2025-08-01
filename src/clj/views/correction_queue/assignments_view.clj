12e3e4674af2c7c82f1c109b13eccb4c6419dfff
jaw <jana.woitaschik@gmail.com>
2025-08-01 13:27:46 +0200

    Added button functionality in assigned question queue

--- src/clj/views/correction_queue/assignments_view.clj
+++ src/clj/views/correction_queue/assignments_view.clj
@@ -37,9 +37,7 @@
                    :value 0}]]
          (h/raw (anti-forgery-field))
          [:div {:class "button-group"}
-          [:button {:type "submit" :name "action" :value "save" :class "btn btn-primary"} "Save"]
-          [:button {:type "submit" :name "action" :value "next" :class "btn btn-secondary"} "Next"]
-          [:button {:type "submit" :name "action" :value "save-next" :class "btn btn-danger"} "Save & Next"]])
+          [:button {:type "submit" :name "action" :value "submit" :class "btn btn-primary"} "Submit correction"]])
         [:p "Question-Statistics: <all-answers> / <corrected-answers> / <corrected-by-you>"]
         [:hr]])]))
   
