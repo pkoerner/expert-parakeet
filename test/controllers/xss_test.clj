74b84c04ae72d1d71fbc581dfc3c6821e710a3e7
DerHyper <maxrichter15@web.de>
2025-08-07 20:22:34 +0200

    Add wip test-xss-create-question

--- test/controllers/xss_test.clj
+++ test/controllers/xss_test.clj
@@ -3,8 +3,16 @@
    [clojure.string :as string]
    [clojure.test :as t :refer [deftest testing]]
    [controllers.course.course-controller :as cc]
+   [controllers.question.question-controller :refer [create-question-get
+                                                     submit-create-question!]]
    [db :refer [Database-Protocol]]
-   [services.course-service.course-service :refer [->CourseService]]))
+   [services.answer-service.answer-service :refer [->AnswerService]]
+   [services.correction-service.correction-service :refer [->CorrectionService]]
+   [services.course-iteration-service.course-iteration-service :refer [->CourseIterationService]] 
+   [services.course-service.course-service :refer [->CourseService]]
+   [services.question-service.question-service :refer [->QuestionService]]
+   [services.question-set-service.question-set-service :refer [->QuestionSetService]]
+   [services.user-service.user-service :refer [->UserService]]))
 
 (def xss-data "<script>alert('XSS')</script>")
 
@@ -26,12 +34,35 @@
    :add-course! (fn [course] (assoc course :course/id 999))))
 
 (def ^:private services
-  {:course-service (->CourseService test-db)})
+  {:course-service (->CourseService test-db)
+   :course-iteration-service (->CourseIterationService test-db)
+   :question-set-service (->QuestionSetService test-db)
+   :question-service (->QuestionService test-db)
+   :answer-service (->AnswerService test-db)
+   :user-service (->UserService test-db)
+   :correction-service (->CorrectionService test-db)})
 
 (deftest test-xss-create-course
-  (testing "Controllers containing html-code should be escaped to prevent XSS."
+  (testing "Create course html-code should be escaped to prevent XSS."
     (let [req {:params {:name xss-data}}
           post-destination "https://some.url"]
       (t/are [html-output] (not (string/includes? html-output xss-data))
         (cc/create-course-get req post-destination)
         (cc/submit-create-course! req post-destination (services :course-service))))))
+
+(deftest test-xss-create-question
+  (testing "Create question html-code should be escaped to prevent XSS."
+    (let [req {:params {:name xss-data}}
+          get-question-categories-fun (fn [] [xss-data])
+          post-destination "https://some.url"]
+      (t/are [html-output] (not (string/includes? html-output xss-data))
+        (create-question-get req get-question-categories-fun post-destination)
+        ;; (submit-create-question! req post-destination (:question-service services))
+        ))))
+
+;; (deftest test-xss-user-overview
+;;   (testing "User overview html-code should be escaped to prevent XSS."
+;;     (let [req {:params {:name xss-data}}
+;;           post-destination "https://some.url"]
+;;       (t/are [html-output] (not (string/includes? html-output xss-data))
+;;         (create-user-overview-get (get-all-course-iterations-for-user (:course-iteration-service services) user-github-id))))))
