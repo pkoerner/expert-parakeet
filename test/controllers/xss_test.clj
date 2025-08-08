688475eacbb0553e8a0e00c4708128f8aec0f57b
DerHyper <maxrichter15@web.de>
2025-08-08 16:38:49 +0200

    Add test-db to xss_test

--- test/controllers/xss_test.clj
+++ test/controllers/xss_test.clj
@@ -3,35 +3,33 @@
    [clojure.string :as string]
    [clojure.test :as t :refer [deftest testing]]
    [controllers.course.course-controller :as cc]
-   [controllers.question.question-controller :refer [create-question-get
-                                                     submit-create-question!]]
+   [datahike.api :as d]
+   [controllers.question.question-controller :refer [create-question-get submit-create-question!]]
    [db :refer [Database-Protocol]]
+   [db.xss-dummy-data :as xss-dummy-data :refer [xss-payload]]
+   [db.schema :refer [db-schema]]
    [services.answer-service.answer-service :refer [->AnswerService]]
    [services.correction-service.correction-service :refer [->CorrectionService]]
-   [services.course-iteration-service.course-iteration-service :refer [->CourseIterationService]] 
+   [services.course-iteration-service.course-iteration-service :refer [->CourseIterationService]]
    [services.course-service.course-service :refer [->CourseService]]
    [services.question-service.question-service :refer [->QuestionService]]
    [services.question-set-service.question-set-service :refer [->QuestionSetService]]
    [services.user-service.user-service :refer [->UserService]]))
 
-(def xss-data "<script>alert('XSS')</script>")
 
-(defn- mock-db
-  [& {:keys [get-all-courses add-course!]
-      :or {get-all-courses (fn [& _] {})
-           add-course! (fn [& _] {})}}]
-  (reify Database-Protocol
-    (get-all-courses
-      [_self]
-      (get-all-courses))
-    (add-course!
-      [_self course]
-      (add-course! course))))
+(defn create-test-db
+  [id]
+  (let [_ (d/create-database {:store {:backend :mem
+                                      :id id}
+                              :initial-tx db-schema})
+        test-db-conn (d/connect {:store {:backend :mem
+                                         :id id}
+                                 :initial-tx db-schema})
+        _ (d/transact test-db-conn xss-dummy-data/xss-data)
+        test-db (db/->Database test-db-conn)]
+    test-db))
 
-(def test-db
-  (mock-db
-   :get-all-courses (fn [] [{:course/id 1 :course/name xss-data}])
-   :add-course! (fn [course] (assoc course :course/id 999))))
+(def test-db (create-test-db "xss-test-db"))
 
 (def ^:private services
   {:course-service (->CourseService test-db)
@@ -44,20 +42,20 @@
 
 (deftest test-xss-create-course
   (testing "Create course html-code should be escaped to prevent XSS."
-    (let [req {:params {:name xss-data}}
+    (let [req {:params {:name xss-payload}}
           post-destination "https://some.url"]
-      (t/are [html-output] (not (string/includes? html-output xss-data))
+      (t/are [html-output] (not (string/includes? html-output xss-payload))
         (cc/create-course-get req post-destination)
         (cc/submit-create-course! req post-destination (services :course-service))))))
 
 (deftest test-xss-create-question
   (testing "Create question html-code should be escaped to prevent XSS."
-    (let [req {:params {:name xss-data}}
-          get-question-categories-fun (fn [] [xss-data])
+    (let [req {:params {:name xss-payload}}
+          get-question-categories-fun (fn [] [xss-payload])
           post-destination "https://some.url"]
-      (t/are [html-output] (not (string/includes? html-output xss-data))
+      (t/are [html-output] (not (string/includes? html-output xss-payload))
         (create-question-get req get-question-categories-fun post-destination)
-        ;; (submit-create-question! req post-destination (:question-service services))
+        (submit-create-question! req post-destination (:question-service services))
         ))))
 
 ;; (deftest test-xss-user-overview
